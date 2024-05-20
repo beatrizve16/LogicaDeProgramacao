d1f3237b6ab453f4a07eb8559d5683834384e700
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-05-20 20:06:59 -0300

    Add files via upload

---
+++ LogicaMatriz/ex_03_7_mat_ta_22408435_beatriz_espindola.c
@@ -0,0 +1,50 @@
+// 3. Construa um programa que efetue a leitura, 
+// a soma posicional e a impressão do resultado,
+//  entre duas matrizes inteiras que comportem 25 elementos.
+//   Use uma terceira matriz para armazenar o resultado.
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void main(void){ 
+
+int i, j;
+int matriz1[5][5];
+int matriz2[5][5];
+int soma[5][5];
+
+printf("Matriz 1\n");
+
+for (j=0;j<5;j++){
+    for (i=0;i<5;i++){ 
+ printf("\nDigite o numero da coluna  %d linha %d: ",j,i);
+ scanf("%d", &matriz1[i][j]);
+		}
+	}
+
+    printf("Matriz 2\n");
+
+for (j=0;j<5;j++){
+    for (i=0;i<5;i++){ 
+ printf("\nDigite o numero da coluna  %d linha %d: ",j,i);
+ scanf("%d", &matriz2[i][j]);
+		}
+	}
+
+       for (i = 0; i < 5; i++) {
+            for (j=0;j<5;j++){
+        soma[i][j] = (matriz1[i][j] + matriz2[i][j]);
+    }
+       }
+
+for (i=0;i<5;i++){ 
+    for (j=0;j<5;j++){
+ printf("[%d]", soma[i][j]);
+		}
+        printf("\n");
+	}
+
+
+
+}
---
+++ LogicaMatriz/ex_04_7_mat_ta_22408435_beatriz_espindola.c
@@ -0,0 +1,58 @@
+// Somar uma matriz por linha, ou seja, obter o total de cada linha da matriz. No final gere
+// uma tela de saída mostrando esses valores e o total geral de toda a matriz. Use uma matriz 4 x 5.
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void main(void)
+{
+
+    int i, j;
+    int matriz1[4][5];
+
+    int soma1 = 0;
+    int soma2 = 0;
+    int soma3 = 0;
+    int soma4 = 0;
+    int soma5 = 0;
+
+    printf("Matriz 1\n");
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            printf("\nDigite o numero da linha  %d coluna %d: ", i, j);
+            scanf("%d", &matriz1[i][j]);
+
+            if (i == 0)
+            {
+                soma1 += matriz1[i][j];
+            }
+
+            if (i == 1)
+            {
+                soma2 += matriz1[i][j];
+            }
+
+            if (i == 2)
+            {
+                soma3 += matriz1[i][j];
+            }
+
+            if (i == 3)
+            {
+                soma4 += matriz1[i][j];
+            }
+       
+        }
+    }
+
+   
+
+    printf("Soma da linha 0=%d\n", soma1);
+    printf("Soma da linha 1=%d\n", soma2);
+    printf("Soma da linha 2=%d\n", soma3);
+    printf("Soma da linha 3=%d\n", soma4);
+
+}
